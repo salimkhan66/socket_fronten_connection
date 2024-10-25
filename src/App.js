0bef7f42c228b07f75548a78feb1a770f61131df
saim khan <salim.khan@mobulous.com>
2024-10-25 12:58:32 +0530

    add connection file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 function App() {
+  const [chatSocket, setChatSocket] = useState(null);
+  const [connected, setConnected] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const connectChat = () => {
+    const socket = io.connect('http://localhost:8080/chat');
+
+    socket.on('connect', () => {
+      console.log('Connected to the /chat namespace');
+      setConnected(true); // Update connection status
+    });
+
+    socket.on('rcv_message', (msg) => {
+      setMessage(msg);
+    });
+
+    setChatSocket(socket);
+  };
+
+  const sendMessage = () => {
+    if (chatSocket) {
+      console.log('Sending message to the /chat namespace');
+      chatSocket.emit('message', 'Hello from the chat namespace!');
+    } else {
+      console.log('Socket not connected');
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+    
+      if (chatSocket) {
+        chatSocket.disconnect();
+      }
+    };
+  }, [chatSocket]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={connectChat}>Connect</button>
+      <button onClick={sendMessage} disabled={!connected}>Send Message</button>
+      <button onClick={()=>setConnected(false)}>Disconnect </button>
+      <h1>{message}</h1>
     </div>
   );
 }
